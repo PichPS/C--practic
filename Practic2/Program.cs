8048df0ff3c6446501dbff131d0e7a609057e841
Pavel_Pichugin <pichps@mail.ru>
2023-01-30 10:38:24 +0300

    Zad-1

---
+++ Practic2/Program.cs
@@ -0,0 +1,17 @@
+﻿int num = new Random().Next(10, 100);
+
+Console.WriteLine (num);
+int num1 = num / 10;
+int num2 = num % 10;
+
+Console.WriteLine(num);
+
+
+if (num1 > num2)
+{
+Console.WriteLine(num1);
+}
+else
+{
+Console.WriteLine(num2);
+}
