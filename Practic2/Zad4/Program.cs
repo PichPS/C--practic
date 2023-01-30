0fcd272959755da8ac42ae72f75a0af8b197bf45
Pavel_Pichugin <pichps@mail.ru>
2023-01-30 15:39:55 +0300

    Сохранение всех задач практики 2

---
+++ Practic2/Zad4/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, 
+//является ли одно число квадратом другого.
+// 5, 25  ->  да
+// -4, 16  ->  да
+// 25, 5  ->  да
+// 8,9  ->  нет
+
+System.Console.WriteLine("Введите первое число: ");
+int numb1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите второе число: ");
+int numb2 = Convert.ToInt32(Console.ReadLine());
+
+if ((numb2 == Math.Pow(numb1, 2)) || (numb1 == Math.Pow(numb2, 2)))
+{
+  Console.WriteLine("Да");
+}
+else
+{
+  Console.WriteLine("Нет");
+}
+
